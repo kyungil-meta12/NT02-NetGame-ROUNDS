82a84932dcb3788d0d479d193710818323e7c2ef
TU-PJY <viska0319@icloud.com>
2026-05-04 14:54:16 +0900

    플레이어 이동 및 점프 추가

--- Assets/___PROJECT/2___Objects/___Player/Player.cs
+++ Assets/___PROJECT/2___Objects/___Player/Player.cs
@@ -17,14 +17,38 @@ public class Player : MonoBehaviour
     public Sprite[] bodies;
     public Sprite[] hands;
 
+    
+    public float moveForce;
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D rb;
+    private float circleColliderRadius;
+
     private float bodyRotation;
     private float gunRotation;
     private bool lookingLeft;
     private Vector2 gunOffset;
     private Vector2 handOffset;
 
+    private bool moveLeft = false, moveRight = false;
+    private bool jump = false;
+
     private Matrix4x4 handMat = new();
     private Matrix4x4 gunMat = new();
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+        // 총 프리펩이 가지는 GunSpec 컴포넌트에서 스펙을 불러와 적용한다
+        var spec = gun.GetComponent<GunSpec>().spec;
+        gunOffset = spec.gunPositionOffset;
+        handOffset = spec.handPositionOffset;
+
+        // 점프 가능 판정에 사용되는 원 콜라이더 반지름
+        circleColliderRadius = GetComponent<CircleCollider2D>().radius;
+    }
 
     void Start()
     {
@@ -33,25 +57,66 @@ public class Player : MonoBehaviour
         int colorIndex = Random.Range(0, hands.Length);
         bodyRenderer.sprite = bodies[colorIndex];
         foreach (var hr in handRenderer) { hr.sprite = hands[colorIndex]; }
-
-        // 총 프리펩이 가지는 GunSpec 컴포넌트에서 스펙을 불러와 적용한다
-        var spec = gun.GetComponent<GunSpec>().spec;
-        gunOffset = spec.gunPositionOffset;
-        handOffset = spec.handPositionOffset;
     }
 
     void Update()
     {
+        InputMove();
         UpdateBodyRotation();
         UpdateGunRotation();
         UpdateHandRotation();
+    }
+
+    void FixedUpdate()
+    {
+        UpdateMove();
+    }
+
+    void InputMove()
+    {
+        moveLeft = Keyboard.current.aKey.isPressed;
+        moveRight = Keyboard.current.dKey.isPressed;
+        jump = Keyboard.current.spaceKey.isPressed;
+    }
+
+    void UpdateMove()
+    {
+        // 좌우 이동
+        if(moveLeft != moveRight)
+        {
+            if(moveLeft)
+            {
+                rb.AddForce(Vector2.left * moveForce, ForceMode2D.Force);
+            }
+            else if(moveRight)
+            {
+                rb.AddForce(Vector2.right * moveForce, ForceMode2D.Force);
+            }
+
+            // 속도 제한
+             rb.linearVelocityX = Mathf.Clamp(rb.linearVelocityX, -moveSpeed, moveSpeed);
+        }
+        else
+        {
+            rb.linearVelocityX = Mathf.Lerp(rb.linearVelocityX, 0f, Time.fixedDeltaTime * 5f);
+        }
+
+        // 점프 // 땅에 닿으면 점프 가능
+        if(jump)
+        {
+            var groundMask = 1 << LayerMask.NameToLayer("Ground");
+            if (Physics2D.Raycast(body.position, Vector2.down, circleColliderRadius + 0.01f, groundMask))
+            {
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+        }
     }
 
     // 몸통 좌우 회전 업데이트
     void UpdateBodyRotation()
     {
         lookingLeft = MouseManager.Inst.worldPos.x < transform.position.x;
-        bodyRotation = Mathf.Lerp(bodyRotation, lookingLeft ? 180f : 0f, Time.deltaTime * 10f);
+        bodyRotation = Mathf.Lerp(bodyRotation, lookingLeft ? 180f : 0f, Time.deltaTime * 5f);
         body.rotation = Quaternion.Euler(new Vector3(0f, bodyRotation, 0f));
     }
 
