ac03cdf2a9b1594e39ff34597194c715174e05b6
Junho <dlatlrhd@gmail.com>
2026-05-08 15:04:32 +0900

    카드 선택 및 확정 시스템 구현, 카드별 능력 설정 및 스프라이트 적용 필요, 라운드에 맞게 씬 전환 중계 기능 필요

--- Assets/___PROJECT/4___Prefabs/___Card/CardSetter.cs
+++ Assets/___PROJECT/4___Prefabs/___Card/CardSetter.cs
@@ -8,12 +8,22 @@ using Random = UnityEngine.Random;
 //카드셀렉트씬 카드 배치용 클래스
 public class CardSetter : MonoBehaviour
 {
+    public TextMeshProUGUI selectedNumberText;
+    
     [Tooltip("4장의 카드 UI 세팅 필요")]
     public CardUI[] cardUI;
     
     [Tooltip("최소 4장 이상의 카드 정보 세팅 필요")]
     public CardInfo[] cards;
-    
+
+    private int[] randomIndices;
+    private int selectedIndex = -1;
+
+    private void Awake()
+    {
+        randomIndices = new int[cardUI.Length];
+    }
+
     /// <summary>
     /// 카드 배열에서 4개를 선택해서 랜덤배치
     /// </summary>
@@ -21,17 +31,33 @@ public class CardSetter : MonoBehaviour
     {
         if (cards == null || cards.Length < 4) return;
         
-        int[] selectedIndices = Enumerable.Range(0, cards.Length)
+        randomIndices = Enumerable.Range(0, cards.Length)
             .OrderBy(x => Random.value)
             .Take(4)
             .ToArray();
         
-        for (int i = 0; i < selectedIndices.Length; i++)
+        for (int i = 0; i < randomIndices.Length; i++)
         {
-            cardUI[i].image.sprite = cards[selectedIndices[i]].image;
-            cardUI[i].titleText.text = cards[selectedIndices[i]].titleText;
-            cardUI[i].descriptionText.text = cards[selectedIndices[i]].descriptionText;
+            cardUI[i].image.sprite = cards[randomIndices[i]].image;
+            cardUI[i].titleText.text = cards[randomIndices[i]].titleText;
+            cardUI[i].descriptionText.text = cards[randomIndices[i]].descriptionText;
         }
+    }
+
+    //카드 선택
+    public void SelectCard(int index)
+    {
+        selectedIndex = index;
+        selectedNumberText.text = $"{index + 1} 번째 카드";
+    }
+
+    //카드 확정
+    public void ConfirmCard()
+    {
+        //카드 선택
+        Debug.Log($"선택된 카드는 {cards[randomIndices[selectedIndex]].titleText}");
+        //todo 카드별 능력 적용필요
+        //todo 다음 게임 씬으로 연결 필요
     }
 }
 
