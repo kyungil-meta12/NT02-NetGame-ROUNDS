73469d74e1d1d651bcd72ea8176b40b00a36dbdb
Junho <dlatlrhd@gmail.com>
2026-05-18 09:48:19 +0900

    hpBar 일반 슬라이더로 변경

--- Assets/___PROJECT/2___Objects/___Player/HpBar.cs
+++ Assets/___PROJECT/2___Objects/___Player/HpBar.cs
@@ -1,29 +1,39 @@
-using MoreMountains.Tools;
-using UnityEngine;
 
-public class HpBar : MMProgressBar
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpBar : MonoBehaviour
 {
+    public Slider hpSlider;
+    
     float maxValue;
 
     public void SetTotalHp(int hp)
     {
         float inputHp = (float)hp;
         maxValue = inputHp;
-        SetBar(inputHp, 0f, inputHp);
+        
+        //SetBar(inputHp, 0f, inputHp);
+        hpSlider.maxValue = maxValue;
+        hpSlider.value = inputHp;
     }
 
     public void SetCurrentHp(int prev, int curr)
     {
         float diff = (float)(prev - curr);
-        float newProgress = BarTarget - diff / maxValue;
-        newProgress = Mathf.Clamp(newProgress, 0f, 1f);
-        UpdateBar01(newProgress);
+        
+        // float newProgress = BarTarget - diff / maxValue;
+        // newProgress = Mathf.Clamp(newProgress, 0f, 1f);
+        // UpdateBar01(newProgress);
+        hpSlider.value = maxValue - diff / maxValue;
     }
-
-    public void TakeDamage(int damage)
+    
+    [ContextMenu("데미지 테스트")]
+    public void TakeDamage()
     {
-        float newProgress = BarTarget - (float)damage / maxValue;
-        newProgress = Mathf.Clamp(newProgress, 0f, 1f);
-        UpdateBar01(newProgress);
+        // float newProgress = BarTarget - 0.1f;
+        // newProgress = Mathf.Clamp(newProgress, 0f, 1f);
+        // UpdateBar01(newProgress);
+        hpSlider.value -= 0.1f;
     }
 }
